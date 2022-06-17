0e90a55da2ea3060afc240bba9034f96e260bedf
Will <wveatchw@gmail.com>
2022-06-17 12:53:14 -0400

    database stuff

--- app/Database/Tiles.hs
+++ app/Database/Tiles.hs
@@ -3,14 +3,27 @@
 
 module Database.Tiles where
 
-import Control.Applicative
-import Data.Text
-import Text.RawString.QQ
-import Data.Typeable
+import Utils
+
+import Control.Applicative ()
+import Data.Text ( Text )
+import Text.RawString.QQ ( r )
+import Data.Typeable ( Typeable )
 
 import Database.SQLite.Simple
-import Database.SQLite.Simple.FromField
-import Database.SQLite.Simple.Types
+    ( close,
+      execute,
+      execute_,
+      open,
+      query,
+      query_,
+      field,
+      Only(Only),
+      FromRow(..),
+      Connection,
+      ToRow(..),
+      Query )
+import Database.SQLite.Simple.Types ( Null(..) )
 import Control.Exception (Exception, throwIO)
 
 data Tile = Tile {
@@ -53,13 +66,15 @@ CREATE TABLE IF NOT EXISTS tiles
    tileWeight INTEGER)
 |]
 
+-- Insert a new tile
 insertTile :: Query
-insertTile = "INSERT INTO users\
-             \ VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+insertTile = "INSERT INTO tiles VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
+-- Get all the tiles
 allTiles :: Query
 allTiles = "SELECT * from tiles"
 
+-- Get all fields from a tile with a certain name
 getTileQuery :: Query
 getTileQuery = "SELECT * from tiles where name = ?"
 
@@ -69,6 +84,8 @@ instance Exception DuplicateData
 
 type TileRow = (Null, Text, Text, Int, Int, Int, Int, Int, Int)
 
+-- Gets a possible tile from a tile name
+-- This is an example. Not something Ill use probably
 getTile :: Connection -> Text -> IO (Maybe Tile)
 getTile conn name = do
   results <- query conn getTileQuery (Only name)
@@ -77,13 +94,62 @@ getTile conn name = do
     [tile] -> return $ Just tile
     _ -> throwIO DuplicateData
 
+data Rotation = None | Once | Twice | Thrice
+
+createTileRow :: Text -> Text -> Rotation -> Int -> Int -> Int -> Int -> Int -> [TileRow]
+createTileRow name path rot l r u d w =
+  case rot of
+    None   -> [ (Null, name, path, 0, l, r, u, d, w) ]
+    Once   -> [ (Null, name, path, 0, l, r, u, d, w)
+              , (Null, name, path, 90, l, r, u, d, w) ]
+    Twice  -> [ (Null, name, path, 0, l, r, u, d, w)
+              , (Null, name, path, 90, l, r, u, d, w)
+              , (Null, name, path, 180, l, r, u, d, w) ]
+    Thrice -> [ (Null, name, path, 0, l, r, u, d, w)
+              , (Null, name, path, 90, l, r, u, d, w)
+              , (Null, name, path, 180, l, r, u, d, w)
+              , (Null, name, path, 270, l, r, u, d, w) ]
+
+rotGetLine :: IO Rotation
+rotGetLine = do
+  n <- intGetLine
+  case n of
+    0 -> return None
+    1 -> return Once
+    2 -> return Twice
+    3 -> return Thrice
+    _ -> do
+      putStrLn "Invalid input."
+      rotGetLine
+
+getInsertTile :: Connection -> IO ()
+getInsertTile conn = do
+  myPutStr "Tile name: "
+  name <- textGetLine
+  myPutStr "Path to tile file: "
+  path <- textGetLine
+  putStrLn "How may times should tile be rotated?"
+  myPutStr "0, 1, 2, or 3: "
+  rot <- rotGetLine
+  myPutStr "Left connector: "
+  l <- intGetLine
+  myPutStr "Right connector: "
+  r <- intGetLine
+  myPutStr "Up connector: "
+  u <- intGetLine
+  myPutStr "Down connector: "
+  d <- intGetLine
+  myPutStr "Tile weight: "
+  w <- intGetLine
+  foldMap (execute conn insertTile) $ createTileRow name path rot l r u d w
+
 creatDatabase :: IO ()
 creatDatabase = do
   conn <- open "test.db"
   execute_ conn createTiles
-  execute conn insertTile meRow
+  execute conn insertTile myRow
   rows <- query_ conn allTiles
   mapM_ print (rows :: [Tile])
   close conn
-  where meRow :: TileRow
-        meRow = (Null, "Test Tile", "not a real path", 0, 1, 1, 1, 1, 10)
+  where myRow :: TileRow
+        myRow = (Null, "Test Tile", "not a real path", 0, 1, 1, 1, 1, 10)
