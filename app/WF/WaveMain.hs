9b4bcda5932c78e475c3555567ddc8ff6d56fd25
Will <wveatchw@gmail.com>
2022-06-18 20:48:54 -0400

    fixes

--- app/WF/WaveMain.hs
+++ app/WF/WaveMain.hs
@@ -1,12 +1,26 @@
 module WF.WaveMain where
 
 import WF.Wave
-import WF.Propagate
+    ( minEntropy,
+      updateGrid,
+      chooseTile,
+      chooseCell,
+      initialGrid,
+      Grid,
+      entropyIndices,
+      updateTile,
+      printGrid, Length, Width )
+import WF.Propagate ( propagate )
+import Utils
+import Database.Tiles
+
+import qualified Data.Vector as V
+import System.Random (randomRIO)
 
 -- A method to choose the first tile.
 -- Eventually add alternative methods that allow user
 -- to choose the first tile.
-firstCollapse :: IO Grid
+firstCollapse :: IO ((Width, Length), Grid)
 firstCollapse = do
   myPutStr "Horizontal number of tiles (width): "
   w <- intGetLine
@@ -16,7 +30,7 @@ firstCollapse = do
   let grid = initialGrid w l tiles
   cell <- chooseCell w l
   n <- chooseTile $ length tiles
-  return $ propagate cell $ updateGrid n cell grid
+  return ((w,l),propagate [cell] w l $ updateGrid n cell grid)
 
 test = do
   myPutStr "Horizontal number of tiles (width): "
@@ -27,13 +41,13 @@ test = do
   let grid = initialGrid w l tiles
   cell <- chooseCell w l
   n <- chooseTile $ length tiles
-  printGrid $ propagate cell $ updateGrid n cell grid
+  printGrid $ propagate [cell] w l $ updateGrid n cell grid
 
 
 -- THE WAVE FUNCTION!!!!!
 -- Repeat until all entropy is 0 or failure.
-wave :: Grid -> IO Grid
-wave grid = do
+wave :: Width -> Length -> Grid -> IO Grid
+wave w l grid = do
   -- Determine the minimun entropy in the grid
   case minEntropy grid of
     -- When min entropy is 0 then its done
@@ -48,11 +62,11 @@ wave grid = do
       -- Collapse the cell and create a new grid
       newGrid <- updateTile cell grid
       -- Propagate the wave through the new grid, updating all cells
-      wave $ propagate cell newGrid
+      wave w l $ propagate [cell] w l newGrid
 
 -- CALL THIS
 waveMain :: IO ()
 waveMain = do
-  grid <- firstCollapse
-  finalGrid <- wave grid
+  trio <- firstCollapse
+  finalGrid <- uncurry (uncurry wave) trio
   printGrid finalGrid
