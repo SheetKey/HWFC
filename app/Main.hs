eb5c3e47274094460c022d31be68f298d1bd6075
Will <wveatchw@gmail.com>
2022-06-23 18:51:44 -0400

    testing

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,18 @@ module Main where
 
 import WF.WaveMain (waveMain)
 import qualified Graphics.Image.IO as I
+import qualified Data.Map as M
 import Image.CreateImage (gridToImage)
+import WF.Wave (gridToOrderedFG, finalGrid)
+import Database.Tiles (Tile(tileId))
+import Data.Maybe (isJust)
 
 main :: IO ()
 main = do
   grid <- waveMain
-  print grid
+  putStrLn "Grid: "
+  print $ M.map tileId <$> finalGrid grid
+  putStrLn "gridToOrderedFG: "
+  print $ (map . map) tileId <$> gridToOrderedFG grid
   image <- gridToImage grid
   I.writeImage "testImage.png" image
